a0e35a1a52f0546190ed78e815ae8e266515c282
goobee <goobee_yuer0716@163.com>
2017-07-19 10:47:08 +0800

    备份基本___

--- app/src/main/java/com/example/administrator/readphp/MainActivity.java
+++ app/src/main/java/com/example/administrator/readphp/MainActivity.java
@@ -109,7 +109,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }).start();
     }
 
-    
+
 
     private void parseJSONWithGSON(String jsonData) {
         try{
